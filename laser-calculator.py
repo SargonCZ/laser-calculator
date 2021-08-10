ba8783746b68ebea6d34c65dbd5c0e2a5457c3bc
SargonCZ <sargoncz@hotmail.com>
2021-08-10 13:29:06 +0200

    First working version

---
+++ laser-calculator.py
@@ -0,0 +1,105 @@
+import pint
+import numpy as np
+import tkinter as tk
+from tkinter import ttk
+import json
+
+class Calculator(ttk.Frame):    
+    def __init__(self,master):
+        ttk.Frame.__init__(self,master)
+        self.master = master
+        self.master.title("Laser calculator")
+
+        with open('functions.json', 'r') as file:
+            self.settings = json.load(file)
+
+        self.ureg = pint.UnitRegistry()
+
+        self.functions = list(self.settings.keys())
+        self.functions_names = [self.settings[x]["name"] for x in self.functions]
+
+        frame_choose_function = ttk.Frame(self)
+        ttk.Label(frame_choose_function,text="Choose a function:").grid(column=0,row=0,sticky="w")
+        self.function_selected = tk.StringVar()
+        self.function_dropdown = ttk.Combobox(frame_choose_function,textvariable=self.function_selected,width=30)
+        self.function_dropdown["values"] = self.functions_names
+        self.function_dropdown.state(["readonly"])
+        self.function_dropdown.grid(row=1,column=0,sticky="ew")
+        self.function_dropdown.bind('<<ComboboxSelected>>', self.on_function_selected)
+        frame_choose_function.columnconfigure(0,weight=1)
+        frame_choose_function.grid(row=0,column=0,sticky="nsew")
+
+
+
+    def on_function_selected(self,event=None):
+        try:
+            self.frame_main.grid_forget()
+            self.frame_main.destroy()
+        except:
+            pass
+
+        fun = self.functions[self.functions_names.index(self.function_selected.get())]
+
+        self.frame_main = ttk.Frame(self)
+        ttk.Label(self.frame_main,text=self.settings[fun]["description"],wraplength=280,justify="left").grid(row=0,column=0,sticky="ew",columnspan=3)
+        self.inputs_units = []
+        self.inputs_values = []
+        self.inputs_CB = []
+        for ind,name in enumerate(self.settings[fun]["inputs"].keys()):
+            ttk.Label(self.frame_main,text=name).grid(row=1+ind,column=0,sticky="w")
+            self.inputs_values.append(tk.StringVar(value=0))
+            ttk.Entry(self.frame_main,textvariable=self.inputs_values[ind]).grid(row=1+ind,column=1,sticky="ew")
+            self.inputs_units.append(tk.StringVar())
+            try: 
+                self.inputs_units[ind].set(value=self.settings[fun]["inputs"][name]["units"][0])
+            except:
+                pass
+            self.inputs_CB.append(ttk.Combobox(self.frame_main,textvariable=self.inputs_units[ind],width=10))
+            self.inputs_CB[ind]["values"] = self.settings[fun]["inputs"][name]["units"]
+            self.inputs_CB[ind].state(["readonly"])
+            self.inputs_CB[ind].grid(row=1+ind,column=2,sticky="e")
+        
+        ttk.Label(self.frame_main,text=f"Units of output ({self.settings[fun]['outputs']['name']})").grid(row=100,column=0,sticky="ew",columnspan=2)
+        self.output = tk.StringVar()        
+        try:
+            self.output.set(value=self.settings[fun]["outputs"]["units"][0])
+        except:
+            pass
+        self.output_CB = ttk.Combobox(self.frame_main,textvariable=self.output,width=10)
+        self.output_CB["values"] = self.settings[fun]["outputs"]["units"]
+        self.output_CB.state(["readonly"])
+        self.output_CB.grid(row=100,column=2,sticky="e")
+
+        ttk.Button(self.frame_main,text="Calculate",command=self.calculate).grid(row=101,column=0,columnspan=3,sticky="ew")
+        self.result = tk.StringVar(value="Result")
+        self.result_number = tk.StringVar()
+        ttk.Label(self.frame_main,textvariable=self.result,font=("Arial", 18)).grid(row=102,column=0,columnspan=3,sticky="ew")
+        ttk.Entry(self.frame_main,textvariable=self.result_number).grid(row=103,column=0,columnspan=3,sticky="ew")
+        self.frame_main.columnconfigure(1,weight=1)
+        self.frame_main.grid(column=0,row=1)
+
+    def calculate(self):
+        fun = self.functions[self.functions_names.index(self.function_selected.get())]
+        I = [None] * len(self.inputs_values)
+        for ind,name in enumerate(self.settings[fun]["inputs"].keys()):
+            try:
+                I[self.settings[fun]["inputs"][name]["position"]] = float(self.inputs_values[ind].get().replace(",",".")) * self.ureg(self.inputs_units[ind].get())  
+            except ValueError:
+                self.result.set(value="Cannot convert to numbers!")
+                return 0
+        function_to_evaluate = self.settings[fun]["function"]        
+        try:
+            result = eval(function_to_evaluate).to(self.output.get())
+            self.result.set(value="{} is {:.3f~P}".format(self.settings[fun]['outputs']['name'],result))
+            self.result_number.set(value=str(result.magnitude))
+        except ZeroDivisionError:
+            self.result.set(value="Cannot divide by zero!")
+            return 0
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Calculator(root)
+    app.grid(column=0,row=0)
+    root.mainloop()
+    
