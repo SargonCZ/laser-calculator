9e6ab7eae347ad55bdaf608a66a5970879b8a158
SargonCZ <sargoncz@hotmail.com>
2021-08-12 14:03:01 +0200

    Added solve (Issue #2)

--- laser-calculator.py
+++ laser-calculator.py
@@ -3,6 +3,7 @@ import numpy as np
 import tkinter as tk
 from tkinter import ttk
 import json
+import sympy
 
 class Calculator(ttk.Frame):    
     def __init__(self,master):
@@ -46,7 +47,42 @@ class Calculator(ttk.Frame):
             ttk.Label(self.frame_main,image=self.formula_preview).grid(row=1,column=0,sticky="ew",columnspan=3)
         except:
             pass
+        
+        if self.settings[fun]["regime"] == "calc":
+            self.build_calc(fun)
+        elif self.settings[fun]["regime"] == "solve":
+            self.build_solve(fun)
+        else:
+            self.write("Something is wrong with the definition.")
 
+    def build_solve(self,fun):
+        self.var_units = []
+        self.var_values = []
+        self.var_CB = []
+        for ind,name in enumerate(self.settings[fun]["variables"].keys()):
+            ttk.Label(self.frame_main,text=self.settings[fun]["variables"][name]["name"]).grid(row=2+ind,column=0,sticky="w")
+            self.var_values.append(tk.StringVar(value=0))
+            ttk.Entry(self.frame_main,textvariable=self.var_values[ind]).grid(row=2+ind,column=1,sticky="ew")
+            self.var_units.append(tk.StringVar())
+            try: 
+                self.var_units[ind].set(value=self.settings[fun]["variables"][name]["units"][0])
+            except:
+                pass
+            self.var_CB.append(ttk.Combobox(self.frame_main,textvariable=self.var_units[ind],width=10))
+            self.var_CB[ind]["values"] = self.settings[fun]["variables"][name]["units"]
+            self.var_CB[ind].state(["readonly"])
+            self.var_CB[ind].grid(row=2+ind,column=2,sticky="e")
+        
+        ttk.Button(self.frame_main,text="Solve",command=self.calculate_btn).grid(row=101,column=0,columnspan=3,sticky="ew")
+        self.result = tk.StringVar(value="Result")
+        self.result_number = tk.StringVar()
+        ttk.Label(self.frame_main,textvariable=self.result,font=("Arial", 18),wraplength=280).grid(row=102,column=0,columnspan=3,sticky="ew")
+        ttk.Entry(self.frame_main,textvariable=self.result_number).grid(row=103,column=0,columnspan=3,sticky="ew")
+        self.frame_main.columnconfigure(1,weight=1)
+        self.frame_main.grid(column=0,row=1)
+
+
+    def build_calc(self,fun):
         self.inputs_units = []
         self.inputs_values = []
         self.inputs_CB = []
@@ -75,7 +111,7 @@ class Calculator(ttk.Frame):
         self.output_CB.state(["readonly"])
         self.output_CB.grid(row=100,column=2,sticky="e")
 
-        ttk.Button(self.frame_main,text="Calculate",command=self.calculate).grid(row=101,column=0,columnspan=3,sticky="ew")
+        ttk.Button(self.frame_main,text="Calculate",command=self.calculate_btn).grid(row=101,column=0,columnspan=3,sticky="ew")
         self.result = tk.StringVar(value="Result")
         self.result_number = tk.StringVar()
         ttk.Label(self.frame_main,textvariable=self.result,font=("Arial", 18),wraplength=280).grid(row=102,column=0,columnspan=3,sticky="ew")
@@ -83,8 +119,57 @@ class Calculator(ttk.Frame):
         self.frame_main.columnconfigure(1,weight=1)
         self.frame_main.grid(column=0,row=1)
 
-    def calculate(self):
+    def calculate_btn(self):
         fun = self.functions[self.functions_names.index(self.function_selected.get())]
+        if self.settings[fun]["regime"] == "calc":
+            self.calculate(fun)
+        elif self.settings[fun]["regime"] == "solve":
+            self.solve(fun)
+        else:
+            self.write("Something is wrong with the definition.")
+    
+    def solve(self,fun):
+        found_x = False
+        I = [None] * len(self.var_values)
+        str_to_eval = [None] * len(self.var_values)
+        # Deciding, what to solve
+        for ind,name in enumerate(self.settings[fun]["variables"].keys()):
+            I[ind] = sympy.symbols(name)
+            if self.var_values[ind].get().strip() == "x":
+                if found_x == False:
+                    x = I[ind]
+                    resulting_units = self.var_units[ind].get()
+                    resulting_name = self.settings[fun]["variables"][name]["name"]
+                    found_x = True
+                else:
+                    self.write("Too many x's.")
+                    return 0
+            else:
+                str_to_eval[ind] = "{0} = float(self.var_values[{1}].get().replace(',','.')) * self.ureg(self.var_units[{1}].get())".format(name,ind)
+        if not found_x:
+            self.write("One x required.")
+            return 0
+        solution = sympy.solve(eval(self.settings[fun]["function"]),x)
+        solution = str(solution[0])
+        # Creating variables for the solution
+        for string in str_to_eval:
+            try: 
+                if string is not None:
+                    exec(string)
+            except ValueError:
+                self.write("Cannot convert to numbers!")
+                return 0
+        # Solving
+        try:
+            result = eval(solution).to(resulting_units)
+            self.result.set(value="{} is {:.3fP}".format(resulting_name,result))
+            self.result_number.set(value=str(result.magnitude))
+        except ZeroDivisionError:
+            self.write("Cannot divide by zero!")
+            return 0
+
+
+    def calculate(self,fun):
         I = [None] * len(self.inputs_values)
         for ind,name in enumerate(self.settings[fun]["inputs"].keys()):
             value = self.inputs_values[ind].get().replace(",",".")
@@ -93,24 +178,28 @@ class Calculator(ttk.Frame):
                 magnitude = value[0].strip()
                 if len(value) > 1:
                     error = value[1].strip()     
+                    I[self.settings[fun]["inputs"][name]["position"]] = (float(magnitude) * self.ureg(self.inputs_units[ind].get())).plus_minus(float(error)) 
                 else:
                     error = 0           
-                I[self.settings[fun]["inputs"][name]["position"]] = (float(magnitude) * self.ureg(self.inputs_units[ind].get())).plus_minus(float(error)) 
+                    I[self.settings[fun]["inputs"][name]["position"]] = (float(magnitude) * self.ureg(self.inputs_units[ind].get())) 
             except ValueError:
-                self.result.set(value="Cannot convert to numbers!")
+                self.write("Cannot convert to numbers!")
                 return 0
         function_to_evaluate = self.settings[fun]["function"]        
         try:
             result = eval(function_to_evaluate).to(self.output.get())
-            if result.error == 0:
-                self.result.set(value="{} is {:.3fP}".format(self.settings[fun]['outputs']['name'],result.value))
-            else: 
-                self.result.set(value="{} is {:.3fP}".format(self.settings[fun]['outputs']['name'],result))
-            self.result_number.set(value=str(result.value.magnitude))
+            self.result.set(value="{} is {:.3fP}".format(self.settings[fun]['outputs']['name'],result))
+            self.result_number.set(value=str(result.magnitude))
         except ZeroDivisionError:
-            self.result.set(value="Cannot divide by zero!")
+            self.write("Cannot divide by zero!")
+            return 0
+        except TypeError:
+            self.write("Uncertainties not implemented for nonlinear functions.")
             return 0
 
+    def write(self,message="Result"):
+        self.result.set(value=str(message))
+        self.result_number.set(value=str(message))
 
 if __name__ == "__main__":
     root = tk.Tk()
